90689cf721abde8e0a6d45c8a18fe766e88a4468
gushaokui <gushaokui@lightinginn.com>
2017-10-05 12:41:09 +0800

    测试全局忽略是否ok

---
+++ swiftWeibo/swiftWeibo/classes/Model/WJHomeModel.swift
@@ -0,0 +1,13 @@
+//
+//  WJHomeModel.swift
+//  swiftWeibo
+//
+//  Created by 龚文娟 on 17/10/5.
+//  Copyright © 2017年 l. All rights reserved.
+//
+
+import UIKit
+
+class WJHomeModel: NSObject {
+
+}
